9f731503599acc93ead8f4ee8cb15cb9f61d9615
pedro <pedromunhozrosin@gmail.com>
2026-03-31 16:08:08 -0300

    adding project info

---
+++ PicPayChallenge/backEnd-challenge/src/main/java/com/picpay/challenge/BackEndChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.picpay.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndChallengeApplication.class, args);
+	}
+
+}
