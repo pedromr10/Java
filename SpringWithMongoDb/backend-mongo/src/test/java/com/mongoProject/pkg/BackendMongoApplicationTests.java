1827aaada53a21a68b5844b7ee303d8b5f8e3a4e
pedro <pedromunhozrosin@gmail.com>
2026-04-01 09:17:15 -0300

    criacao do projeto spring boot com mongodb

---
+++ SpringWithMongoDb/backend-mongo/src/test/java/com/mongoProject/pkg/BackendMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongoProject.pkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
