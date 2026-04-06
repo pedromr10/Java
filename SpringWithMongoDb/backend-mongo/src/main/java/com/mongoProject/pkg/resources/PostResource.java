8c78ffa6c0954736d2c0029f3b8875dd857d244e
pedro <pedromunhozrosin@gmail.com>
2026-04-06 11:36:10 -0300

    Query with multiple criteria

--- SpringWithMongoDb/backend-mongo/src/main/java/com/mongoProject/pkg/resources/PostResource.java
+++ SpringWithMongoDb/backend-mongo/src/main/java/com/mongoProject/pkg/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.mongoProject.pkg.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,17 @@ public class PostResource {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text){
 		text = URL.decodeParam(text);
 		List<Post> list = postService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+		//para testar: http://localhost:8080/posts/titlesearch?text=bom%20dia
+	}
+	
+	//retorna os posts que contem uma palavra:
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(@RequestParam(value="text", defaultValue = "") String minDate, @RequestParam(value="minDate", defaultValue = "") String text, @RequestParam(value="maxDate", defaultValue = "") String maxDate){
+		text = URL.decodeParam(text);
+		Date min= URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = postService.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 		//para testar: http://localhost:8080/posts/titlesearch?text=bom%20dia
 	}
