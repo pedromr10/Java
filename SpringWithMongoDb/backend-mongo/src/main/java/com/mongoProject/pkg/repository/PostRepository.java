d7b7a91fde4c63fffb02bf650ff4450782021a73
pedro <pedromunhozrosin@gmail.com>
2026-04-06 10:51:04 -0300

    Simple query with query methods

--- SpringWithMongoDb/backend-mongo/src/main/java/com/mongoProject/pkg/repository/PostRepository.java
+++ SpringWithMongoDb/backend-mongo/src/main/java/com/mongoProject/pkg/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.mongoProject.pkg.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.mongoProject.pkg.domain.Post;
 @Repository //acesso ao banco
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- SpringWithMongoDb/backend-mongo/src/main/java/com/mongoProject/pkg/services/PostService.java
+++ SpringWithMongoDb/backend-mongo/src/main/java/com/mongoProject/pkg/services/PostService.java
@@ -1,5 +1,6 @@
 package com.mongoProject.pkg.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,10 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = postRepository.findById(id);
 		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto nao encontrado."));
+	}
 	
+	public List<Post> findByTitle(String text){
+		return postRepository.findByTitleContainingIgnoreCase(text);
 	}
 	
 }
