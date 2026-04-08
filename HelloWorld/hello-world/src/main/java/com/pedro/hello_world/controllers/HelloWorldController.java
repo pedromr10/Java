ceb33376c35f8bed17c7011b4fc82452b27aec38
pedro <pedromunhozrosin@gmail.com>
2026-04-08 18:11:49 -0300

    Adding post method

--- HelloWorld/hello-world/src/main/java/com/pedro/hello_world/controllers/HelloWorldController.java
+++ HelloWorld/hello-world/src/main/java/com/pedro/hello_world/controllers/HelloWorldController.java
@@ -2,9 +2,12 @@ package com.pedro.hello_world.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pedro.hello_world.domain.User;
 import com.pedro.hello_world.services.HelloWorldService;
 
 @RestController
@@ -17,6 +20,11 @@ public class HelloWorldController {
 	@GetMapping
 	public String helloWorld() {
 		return hwService.helloWorld("Pedro");
+	}
+	
+	@PostMapping()
+	public String helloWorldPost(@RequestBody User body) {
+		return "Hello World, " + body.getName() + " - " + body.getEmail();
 	}
 	
 }
---
+++ HelloWorld/hello-world/src/main/java/com/pedro/hello_world/domain/User.java
@@ -0,0 +1,15 @@
+package com.pedro.hello_world.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+
+	private String name;
+	private String email;
+	
+}
