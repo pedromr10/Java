225da59b3b7739b98a9a4521d460169c2bee0619
pedro <pedromunhozrosin@gmail.com>
2026-04-08 17:40:02 -0300

    Adding dev profile, set as active

--- HelloWorld/hello-world/src/main/java/com/pedro/hello_world/HelloWorldApplication.java
+++ HelloWorld/hello-world/src/main/java/com/pedro/hello_world/HelloWorldApplication.java
@@ -2,8 +2,11 @@ package com.pedro.hello_world;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+
+@Profile("dev")
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
