2842ce1252679c0692d893262e7262d7846d6c0b
pedro <pedromunhozrosin@gmail.com>
2026-04-08 11:19:04 -0300

    Coded transaction function with mock data

--- PicPayChallenge/picpaychallenge/src/main/java/com/pedro/picpaychallenge/services/TransferService.java
+++ PicPayChallenge/picpaychallenge/src/main/java/com/pedro/picpaychallenge/services/TransferService.java
@@ -5,8 +5,8 @@ import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.pedro.picpaychallenge.domain.Transaction;
 import com.pedro.picpaychallenge.domain.User;
-import com.pedro.picpaychallenge.domain.enums.UserType;
 import com.pedro.picpaychallenge.repository.TransactionRepository;
 import com.pedro.picpaychallenge.repository.UserRepository;
 
@@ -35,13 +35,39 @@ public class TransferService {
 		if(payer.getBalance().compareTo(value) < 0) {
 			throw new RuntimeException("Insufficient balance");
 		}
+		//verifica se foi autorizado (dado mockado ainda):
 		if(!authorizeTransaction()) {
 			throw new RuntimeException("Transaction not authorized");
+		}
+		
+		//caso nenhuma excecao ocorra, acontece a transferencia:
+		payer.setBalance(payer.getBalance().subtract(value));
+		payee.setBalance(payee.getBalance().add(value));
+		userRepository.save(payer);
+		userRepository.save(payee);
+		
+		//cria e salva a transacao:
+		Transaction transaction = new Transaction();
+		transaction.setAmount(value);
+		transaction.setPayee(payee);
+		transaction.setPayer(payer);
+		transactionRepository.save(transaction);
+		
+		//envia notificacao:
+		try {
+		    sendNotification(payer);
+		    sendNotification(payee);
+		} catch (Exception e) {
+		    System.out.println("Notification failed");
 		}
 	}
 	
     private boolean authorizeTransaction() {
         return true;
+    }
+    
+    private void sendNotification(User user) {
+        System.out.println("Notifying user: " + user.getEmail());
     }
 	
 }
