8139038403e5e3b7725c5c3fcacdc6fb90daa2a0
pedro <pedromunhozrosin@gmail.com>
2026-04-08 18:22:26 -0300

    Adding post with @RequestParam

--- HelloWorld/hello-world/src/main/java/com/pedro/hello_world/controllers/HelloWorldController.java
+++ HelloWorld/hello-world/src/main/java/com/pedro/hello_world/controllers/HelloWorldController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pedro.hello_world.domain.User;
@@ -24,8 +25,8 @@ public class HelloWorldController {
 	}
 	
 	@PostMapping("/{id}")
-	public String helloWorldPost(@PathVariable("id") String id, @RequestBody User body) {
-		return "Hello World, " + body.getName() + " - " + body.getEmail() + " - " + id;
+	public String helloWorldPost(@PathVariable("id") String id, @RequestParam(value = "filter",defaultValue = "none") String filter, @RequestBody User body) {
+		return "Hello World, " + body.getName() + " - " + body.getEmail() + " - " + id + " - " + filter;
 	}
 	
 }
