5751ff2af760e2c7400d2da80d31468a6b82aec5
pedro <pedromunhozrosin@gmail.com>
2026-04-08 18:14:25 -0300

    Adding post with @PathVariable

--- HelloWorld/hello-world/src/main/java/com/pedro/hello_world/controllers/HelloWorldController.java
+++ HelloWorld/hello-world/src/main/java/com/pedro/hello_world/controllers/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.pedro.hello_world.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,9 +23,9 @@ public class HelloWorldController {
 		return hwService.helloWorld("Pedro");
 	}
 	
-	@PostMapping()
-	public String helloWorldPost(@RequestBody User body) {
-		return "Hello World, " + body.getName() + " - " + body.getEmail();
+	@PostMapping("/{id}")
+	public String helloWorldPost(@PathVariable("id") String id, @RequestBody User body) {
+		return "Hello World, " + body.getName() + " - " + body.getEmail() + " - " + id;
 	}
 	
 }
