9b2a40318be91f53eca4ae50c099101aa253ffaf
pedro <pedromunhozrosin@gmail.com>
2026-04-09 10:28:41 -0300

    Adding transaction repository

---
+++ PicPayChallenge/picpaychallenge/src/main/java/com/pedro/picpaychallenge/repositories/TransactionRepo.java
@@ -0,0 +1,11 @@
+package com.pedro.picpaychallenge.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pedro.picpaychallenge.domain.Transaction;
+
+@Repository
+public interface TransactionRepo extends JpaRepository<Transaction, Long>{
+
+}
