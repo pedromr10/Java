f1e400049624d5ba9b4864a8520cc416195b33b5
pedro <pedromunhozrosin@gmail.com>
2026-04-23 13:47:29 -0300

    Coded contact repo, service and controller

--- NubankChallenge/nubankchallenge/src/main/java/com/pedro/nubankchallenge/controllers/ContactController.java
+++ NubankChallenge/nubankchallenge/src/main/java/com/pedro/nubankchallenge/controllers/ContactController.java
@@ -1,12 +1,45 @@
 package com.pedro.nubankchallenge.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pedro.nubankchallenge.dtos.requests.ContactRequestDto;
+import com.pedro.nubankchallenge.dtos.responses.ContactResponseDto;
+import com.pedro.nubankchallenge.services.ContactService;
 
 @RestController
 @RequestMapping("/contacts")
 public class ContactController {
 	
-	@PostMapping
+	@Autowired
+	private ContactService contactService;
+	
+	//cadastro de contato:
+	@PostMapping("/{clientId}")
+	public ResponseEntity<ContactResponseDto> addContact(@RequestBody ContactRequestDto contactRequest, @PathVariable Long clientId){
+		ContactResponseDto contactResponse = contactService.addContact(contactRequest, clientId); 
+		return ResponseEntity.status(201).body(contactResponse);
+	}
+	
+	//listar todos os contatos de um cliente em especifico:
+	@GetMapping("/{clientId}")
+	public ResponseEntity<List<ContactResponseDto>> listContacts(@PathVariable Long clientId){
+		return ResponseEntity.status(200).body(contactService.listContacts(clientId));
+	}
+	
 }
+
+
+
+
+
+
+
--- NubankChallenge/nubankchallenge/src/main/java/com/pedro/nubankchallenge/services/ContactService.java
+++ NubankChallenge/nubankchallenge/src/main/java/com/pedro/nubankchallenge/services/ContactService.java
@@ -1,12 +1,16 @@
 package com.pedro.nubankchallenge.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pedro.nubankchallenge.dtos.requests.ContactRequestDto;
 import com.pedro.nubankchallenge.dtos.responses.ContactResponseDto;
+import com.pedro.nubankchallenge.entities.Client;
 import com.pedro.nubankchallenge.entities.Contact;
 import com.pedro.nubankchallenge.mappers.ContactMapping;
+import com.pedro.nubankchallenge.repositories.ClientRepository;
 import com.pedro.nubankchallenge.repositories.ContactRepository;
 
 @Service
@@ -16,11 +20,23 @@ public class ContactService {
 	private ContactRepository contactRepo;
 	@Autowired
 	private ContactMapping contactMapper;
+	@Autowired
+	private ClientRepository clientRepo;
 	
 	//cadastro de contato:
-	public ContactResponseDto addContact(ContactRequestDto contactRequest) {
-		Contact savedContact = contactRepo.save(contactMapper.toEntity(contactRequest));
+	public ContactResponseDto addContact(ContactRequestDto contactRequest, Long id) {
+		Client client = clientRepo.findById(id).orElseThrow(()-> new RuntimeException("Client not found"));
+		Contact contact = contactMapper.toEntity(contactRequest);
+		contact.setClient(client);
+		client.getContacts().add(contact);
+		Contact savedContact = contactRepo.save(contact);
 		return contactMapper.toResponse(savedContact);
+	}
+	
+	//listar todos os contatos de um cliente em especifico:
+	public List<ContactResponseDto> listContacts(Long id){
+		Client client = clientRepo.findById(id).orElseThrow(()-> new RuntimeException("Client not found"));
+		return client.getContacts().stream().map(contactMapper::toResponse).toList();
 	}
 	
 	
