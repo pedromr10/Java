3ab84a065513f4488973c05be5b37565855ff50e
pedro <pedromunhozrosin@gmail.com>
2026-04-23 08:22:23 -0300

    Added project

---
+++ NubankChallenge/nubankchallenge/src/main/java/com/pedro/nubankchallenge/NubankchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.nubankchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NubankchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NubankchallengeApplication.class, args);
+	}
+
+}
