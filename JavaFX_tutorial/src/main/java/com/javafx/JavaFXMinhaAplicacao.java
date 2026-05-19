9c2c4b1bc772ddb0da150f99943d33b1a50306f4
pedro <pedromunhozrosin@gmail.com>
2026-05-19 10:32:40 -0300

    added label, button and txtfield

--- JavaFX_tutorial/src/main/java/com/javafx/JavaFXMinhaAplicacao.java
+++ JavaFX_tutorial/src/main/java/com/javafx/JavaFXMinhaAplicacao.java
@@ -1,12 +1,25 @@
 package com.javafx;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class JavaFXMinhaAplicacao extends Application{
 	@Override
 	public void start(Stage palco) {
 		palco.setTitle("Meu primeiro palco");
+		Label label = new Label("Ola, mundo! Sou uma label");
+		Button botao = new Button("Clique aqui");
+		TextField campoTexto = new TextField();
+		
+		VBox layout = new VBox(label, botao, campoTexto);
+		
+		Scene cena = new Scene(layout, 300, 200); //300px, 200px
+		palco.setScene(cena);
 		palco.show();
 	}
 	
