ffb3d794db247c285aae3a3aefe81b7348f0c23c
pedro <pedromunhozrosin@gmail.com>
2026-05-19 14:18:57 -0300

    started password project

---
+++ JavaFX_projetos/SenhaSegura/src/main/java/com/javafx/SenhaSegura.java
@@ -0,0 +1,30 @@
+package com.javafx;
+
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class SenhaSegura{
+	
+	private static final String CARACTERES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%¨&*()_+-=[]|,./?><";
+	
+	public static String gerarSenha(int comprimento) {
+		SecureRandom geradorDeNumeroAleatorio = new SecureRandom();
+		StringBuilder senha = new StringBuilder(comprimento);
+		
+		for(int i = 0;i<comprimento;i++) {
+			int indice = geradorDeNumeroAleatorio.nextInt(CARACTERES.length());
+			senha.append(CARACTERES.charAt(indice));
+		}
+		
+		return senha.toString();
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Digite o comprimento da senha que deseja: ");
+		int comprimentoSenha = scanner.nextInt();
+		String senha = gerarSenha(comprimentoSenha);
+		System.out.println("Senha gerada: " + senha);
+	}
+	
+}
